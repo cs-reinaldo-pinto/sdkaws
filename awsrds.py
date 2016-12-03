defeab1fe9c8605f8bfe8d7e3710b9aa89a4ca8e
reinaldo <reinaldo@localhost.localdomain>
2016-12-03 00:49:23 -0200

    Novo commit

--- awsrds.py
+++ awsrds.py
@@ -2,22 +2,40 @@
 
 import boto3
 import time
-
-
+import sys
+import botocore
 #rds = boto3.client(profile_name='dev')
 
 #rds = boto3.setup_default_session(region_name='us-east-1a')
-rds = boto3.client('rds')
-time = time.strftime("%d%m%Y")
+client = boto3.client('rds')
+#time = time.strftime("%d%m%Y")
 
-name_db='db-12-02-01-07'#'testore'
+#name_db='db-12-02-01-07'#'testore'
+
+
+response =  client.describe_db_instances(DBInstanceIdentifier="dbdevconciliador")
+
+response = response['DBInstances']
+
+db = response[0]
+print (db)
+
+status = db['DBInstanceStatus']
+print '\n'+(status)
+
+if status == "available":
+    print ''
+#    dbname = db['DBInstanceIdentifier']
+#    snapshot = client.create_db_client_snapshot(DBSnapshotIdentifier=dbname,DBInstanceIdentifier='sssdbname')
+else:
+    print 'Nao entrou'
 
 
 
 #describeSnapshot
-snapshot = rds.describe_db_snapshot_attributes(
-    DBSnapshotIdentifier='db-2016-12-02-01-07'
-)
+#snapshot = rds.describe_db_snapshot_attributes(
+#    DBSnapshotIdentifier=''
+#)
 
 #create Snapshot
 #create = cliente.create_db_client_snapshot(
