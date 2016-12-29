b60ff38d872e7639f99e8f7722056263d3cc78a6
reinaldo.pinto <reinaldo.pinto>
2016-12-29 20:51:30 -0200

    novo commit

--- s3_aws.py
+++ s3_aws.py
@@ -1,16 +1,31 @@
 #/usr/bin/python
 
-import boto3
+from boto3 import client
 
 #s3=boto3.resource('s3')
 
-name='tes1'
+s3= client('s3')
 
-session = boto3.Session(profile_name='dev')
+#name='testereleng'
+#s3=s3.Bucket(name=name)
 
-s3 = session.client('s3')
-s3.create_bucket(Bucket=name)
+#session = boto3.Session(profile_name='ola')
 
-print (s3.list_objects(Bucket=name))
+#s3 = session.client('s3')
+#s3=s3.Bucket(name)
 
-print (s3.get_bucket_logging(Bucket=name))
+for key in s3.list_objects(Bucket='testereleng')['Contents']:
+    print (key['Key']),(key['LastModified']) 
+    time=key['LastModified']
+
+print (time[])
+#conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
+#for key in conn.list_objects(Bucket='testereleng')['Contents']:
+#    print(key['Key'])
+
+#for objects in s3.objects.all():
+#    print(objects)
+
+#print (s3.list_objects(Bucket=name))
+
+#stModifiedprint (s3.get_bucket_logging(Bucket=name))
