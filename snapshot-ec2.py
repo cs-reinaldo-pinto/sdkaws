867c45cd40eae87b74003cd88310c58be0d47e6b
reinaldo-pinto <reinaldo.souza@icarros.com.br>
2017-10-01 03:36:35 -0300

    update funcao lambda
    Funcao lambda

--- snapshot-ec2.py
+++ snapshot-ec2.py
@@ -1,33 +1,34 @@
 import boto3
 from datetime import datetime,timedelta
 
-client = boto3.client('ec2')
 
-days = -1
-id_owner='account'
-volume_id='vol-id'
-snapshots = client.describe_snapshots(OwnerIds=[id_owner])
-time_now = datetime.now()
+def lambda_handler(event, context):
 
-def delete_snapshot(snapshot_time,delete_snap_id,vol_id):
-    print "==========================================="
-    print "Delete Snapshot ID: '%s' " % (delete_snap_id)
-    print "Datetime:",snapshot_time
-    print "Backup Volume ID: '%s'" % (vol_id)
-    snapshot_resource = boto3.resource('ec2')
-    snapshot = snapshot_resource.Snapshot(delete_snap_id)
-    snapshot.delete()
+    client = boto3.client('ec2')
     
-for snapshot in snapshots['Snapshots']:
-#    print volume_id
-#    print snapshot['VolumeId']
-    snapshot_time = snapshot['StartTime'].replace(tzinfo=None)
-    if (volume_id == snapshot['VolumeId'] and (time_now - snapshot_time) > timedelta(days)):
-        #if (time_now - snapshot_time) > timedelta(days):
-        delete_snap_id = snapshot['SnapshotId']
-        delete_snapshot(snapshot_time,delete_snap_id,snapshot['VolumeId']) 
-    else: 
-        snapshot_id = snapshot['SnapshotId']
+    days = -1
+    id_owner='936675601706'
+    volume_id='vol-059954abdae679ec0'
+    snapshots = client.describe_snapshots(OwnerIds=[id_owner])
+    time_now = datetime.now()
+    
+    def delete_snapshot(snapshot_time,delete_snap_id,vol_id):
         print "==========================================="
-        print "Snapshot id '%s' recent, within of configured limit or different volume ID '%s' than specified." % (snapshot['SnapshotId'],snapshot['VolumeId'])
+        print "Delete Snapshot ID: '%s' " % (delete_snap_id)
         print "Datetime:",snapshot_time
+        print "Backup Volume ID: '%s'" % (vol_id)
+        snapshot_resource = boto3.resource('ec2')
+        snapshot = snapshot_resource.Snapshot(delete_snap_id)
+        snapshot.delete()
+        
+    for snapshot in snapshots['Snapshots']:
+        snapshot_time = snapshot['StartTime'].replace(tzinfo=None)
+        if (volume_id == snapshot['VolumeId'] and (time_now - snapshot_time) > timedelta(days)):
+            delete_snap_id = snapshot['SnapshotId']
+            delete_snapshot(snapshot_time,delete_snap_id,snapshot['VolumeId']) 
+        else: 
+            snapshot_id = snapshot['SnapshotId']
+            print "==========================================="
+            print "Snapshot id '%s' recent, within of configured limit or different volume ID '%s' than specified." % (snapshot['SnapshotId'],snapshot['VolumeId'])
+            print "Datetime:",snapshot_time
+    return
